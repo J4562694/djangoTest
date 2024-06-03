dfe07eab0bab9504ffadd48cb38d41ff8b72f4f0
unknown <eertg123@gmail.com>
2024-06-03 16:58:32 +0800

    將後端移至backend

--- storefront/playground/forms.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django import forms
-from .models import Whitelist
-
-# form表單model
-class WhitelistForm(forms.ModelForm):
-    class Meta:
-        model = Whitelist
-        fields = ['driverName', 'licensePlate']
--- storefront/playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import sayHello, addWhitelistEntry
-
-# URLConf
-urlpatterns = [
-    path('hello/', sayHello, name='sayHello'), 
-    path('addWhitelist/', addWhitelistEntry, name='addWhitelist')
-]
